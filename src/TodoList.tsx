52a0fbad89d2dcd1f78c4eafc21aa74b5dcd094d
Göran Olson <goran@famolson.se>
2024-03-01 16:40:36 +0100

    created boilerplate for edit post

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -8,6 +8,7 @@ export interface ITodoListProps {
     moveUp: (id: number) => void;
     moveDown: (id: number) => void;
     sortPosts: (sort: string) => void;
+    editPost: (id: number) => void;
 }
 
 export function TodoList(props: ITodoListProps): JSX.Element {
@@ -35,6 +36,10 @@ export function TodoList(props: ITodoListProps): JSX.Element {
         // document.activeElement.blur();  // Remove focus
     }
 
+    const handleClickEdit: React.MouseEventHandler<HTMLElement> = (event) => {
+        console.log('--> handleClickUpdate() ' + event.currentTarget.id);
+        props.editPost(parseInt(event.currentTarget.id));
+    }
 
     return (
         <section className='block todo-list'>
@@ -49,20 +54,22 @@ export function TodoList(props: ITodoListProps): JSX.Element {
 
             <section className='todo-content'>
                 {list.map((item) => {
+                    { console.log(item) }
                     const stringId: string = item.id.toString();  // Set id on controllers
                     const classesItem = item.done ? "todo-item done" : "todo-item";  // Mark if todo is done
 
                     return (
                         <div className={classesItem} key={item.id}>
-                            <div className='todoText'>{item.todoText}</div>
+                            <div className='todoText'>{item.todoText} {item.edit ? 'EDIT' : null}</div>
                             <div className='info'>
                                 <div className='data'>
                                     <div>{item.username}</div>
                                     <div>{item.date}</div>
                                 </div>
                                 <div className='controller'>
-                                    <i className="fa fa-arrow-up up" onClick={handleOnClickUp} id={stringId}></i>
-                                    <i className="fa fa-arrow-down down" onClick={handleOnClickDown} id={stringId}></i>
+                                    <i className="fas fa-pen edit" onClick={handleClickEdit} id={stringId}></i>
+                                    <i className="fas fa-arrow-up up" onClick={handleOnClickUp} id={stringId}></i>
+                                    <i className="fas fa-arrow-down down" onClick={handleOnClickDown} id={stringId}></i>
                                     <i className="fas fa-trash-alt remove" onClick={handleOnRemoveClick} id={stringId}></i>
                                     <i className="fas fa-check done" onClick={handleOnClickDone} id={stringId}></i>
                                 </div>
