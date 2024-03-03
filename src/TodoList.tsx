10eb3b2da6de500d40b28604c67f26457b545573
Göran Olson <goran@famolson.se>
2024-03-03 22:03:12 +0100

    created editing functionality

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,5 @@
-import { IData } from './App'
+import { IData, IUpdate } from './App'
+import { EditForm } from './EditForm';
 import './TodoList.css'
 
 export interface ITodoListProps {
@@ -9,36 +10,16 @@ export interface ITodoListProps {
     moveDown: (id: number) => void;
     sortPosts: (sort: string) => void;
     editPost: (id: number) => void;
+    editSave: (item: IUpdate) => void;
+    editCancel: (id: number) => void;
 }
 
 export function TodoList(props: ITodoListProps): JSX.Element {
     const list = props.todoList;
 
-    const handleOnRemoveClick: React.MouseEventHandler<HTMLButtonElement>  = (event) => {
-        props.removePost(parseInt(event.currentTarget.id));
-    };
-
-    const handleOnClickDone: React.MouseEventHandler<HTMLButtonElement>  = (event) => {
-        props.donePost(parseInt(event.currentTarget.id));
-    }
-
-    const handleOnClickUp: React.MouseEventHandler<HTMLButtonElement>  = (event) => {
-        props.moveUp(parseInt(event.currentTarget.id));
-    }
-
-    const handleOnClickDown: React.MouseEventHandler<HTMLButtonElement>  = (event) => {
-        props.moveDown(parseInt(event.currentTarget.id));
-    }
-
     const handleSort: React.ChangeEventHandler<HTMLSelectElement> = (event) => {
-        // console.log('sortOrder: ' + event.target.value);
         props.sortPosts(event.target.value);
-        // document.activeElement.blur();  // Remove focus
-    }
-
-    const handleClickEdit: React.MouseEventHandler<HTMLElement> = (event) => {
-        console.log('--> handleClickUpdate() ' + event.currentTarget.id);
-        props.editPost(parseInt(event.currentTarget.id));
+        // document.activeElement.blur();  // Remove focus...?
     }
 
     return (
@@ -48,30 +29,32 @@ export function TodoList(props: ITodoListProps): JSX.Element {
                 <select name="sortOrder" id="sortOrder" onChange={handleSort}>
                     {/* <option value="">Sort order</option> */}
                     <option value="author">author</option>
-                    <option value="latest" selected>latest</option>
+                    <option value="newest" selected>newest</option>
                 </select>
             </header>
 
             <section className='todo-content'>
                 {list.map((item) => {
-                    { console.log(item) }
-                    const stringId: string = item.id.toString();  // Set id on controllers
                     const classesItem = item.done ? "todo-item done" : "todo-item";  // Mark if todo is done
 
                     return (
                         <div className={classesItem} key={item.id}>
-                            <div className='todoText'>{item.todoText} {item.edit ? 'EDIT' : null}</div>
+                            <div className='todoText'> 
+                            { item.edit 
+                                ? <EditForm text={item.todoText} id={item.id}  editSave={props.editSave} editCancel={props.editCancel} /> 
+                                : item.todoText}</div>
+                                
                             <div className='info'>
                                 <div className='data'>
                                     <div>{item.username}</div>
                                     <div>{item.date}</div>
                                 </div>
                                 <div className='controller'>
-                                    <i className="fas fa-pen edit" onClick={handleClickEdit} id={stringId}></i>
-                                    <i className="fas fa-arrow-up up" onClick={handleOnClickUp} id={stringId}></i>
-                                    <i className="fas fa-arrow-down down" onClick={handleOnClickDown} id={stringId}></i>
-                                    <i className="fas fa-trash-alt remove" onClick={handleOnRemoveClick} id={stringId}></i>
-                                    <i className="fas fa-check done" onClick={handleOnClickDone} id={stringId}></i>
+                                    <i className="fas fa-pen edit" onClick={() => {props.editPost(item.id)} }></i>
+                                    <i className="fas fa-arrow-up up" onClick={() => {props.moveUp(item.id)} }></i>
+                                    <i className="fas fa-arrow-down down" onClick={() => {props.moveDown(item.id)} }></i>
+                                    <i className="fas fa-trash-alt remove" onClick={() => {props.removePost(item.id)} }></i>
+                                    <i className="fas fa-check done" onClick={() => {props.donePost(item.id)} }></i>
                                 </div>
                             </div>
                         </div> 
